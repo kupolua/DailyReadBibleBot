9555d40fc0308d6c7b7f8bb5f12c7010398d10e5
Pavel Kulakovsky <pkulakovsky@gmail.com>
2018-03-02 09:46:24 +0200

    Leap Year bug fixed

--- notifier/BibleOneYearNewTestamentNotifierDebugger/getDayNumber.js
+++ notifier/BibleOneYearNewTestamentNotifierDebugger/getDayNumber.js
@@ -1,3 +1,6 @@
+var isLeapYear = function (year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+};
 var getToday = function() {
     var now = new Date();
     var start = new Date(now.getFullYear(), 0, 0);
@@ -5,7 +8,7 @@ var getToday = function() {
     var oneDay = 1000 * 60 * 60 * 24;
     var day = Math.floor(diff / oneDay).toString();
 
-    return day;
-}
+    return isLeapYear(now.getFullYear()) ? day : (parseInt(day) + 1).toString();
+};
 
 module.exports = getToday();
